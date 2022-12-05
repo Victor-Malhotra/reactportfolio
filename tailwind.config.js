52df4253fd77635b4b8d4b67ff7033e2a72710b2
Victor Malhotra <victorrmalhotra@gmail.com>
2022-12-05 15:19:44 -0700

    A

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    aspectRatio: {
-      none: 0,
-      square: [1, 1],
-      "16/9": [16, 9],
-      "4/3": [4, 3],
-      "21/9": [21, 9],
-    },
-  },
-  variants: {
-    aspectRatio: ["responsive"],
     extend: {},
   },
-  plugins: [
-    require("tailwindcss-aspect-ratio"),
-    require("tailwindcss-responsive-embed"),
-  ],
+  plugins: [],
 };
